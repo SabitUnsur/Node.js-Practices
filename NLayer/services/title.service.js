0b2aca2462f134b0fac3632a2b69e6dacea301b7
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-05 16:57:30 +0300

    title cruds

--- NLayer/services/title.service.js
+++ NLayer/services/title.service.js
@@ -33,3 +33,50 @@ exports.listTitles = async () => {
         throw new Error(error)
     }
 }
+
+
+exports.getTitleById = async (req) => { 
+    try {
+        const { id } = req.params
+        const json = await titleDal.title.getTitleById(id)
+        return {
+            ...titleDto,
+            name: json.name,
+            id: json.id,
+            createdAt: json.createdAt,
+            updatedAt: json.updatedAt
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+exports.updateTitle = async (req) => { 
+    try {
+        const { name } = req.body
+        const { id } = req.params
+        const json = await titleDal.title.updateById(id, { name })
+        return {
+            ...titleDto,
+            name: name,
+            id: json.id,
+            createdAt: json.createdAt,
+            updatedAt: json.updatedAt
+        }
+
+    } catch (error) {
+        throw new Error(error) // throw error to be caught by the controller
+    }
+}
+
+exports.deleteTitleById = async (req) => { 
+    try {
+        const { id } = req.query
+        console.log(id)
+        const json = await titleDal.title.deleteById(id)
+        return {...titleDto,name : json.name, id: json.id, createdAt: json.createdAt, updatedAt: json.updatedAt}
+    }catch (error) {
+        throw new Error(error) 
+    }
+
+}
