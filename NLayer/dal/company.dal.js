2304a4b0ddd73b5ca7b405f620eeefc84cfddfd1
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-05 12:30:00 +0300

    company ops

--- NLayer/dal/company.dal.js
+++ NLayer/dal/company.dal.js
@@ -6,6 +6,15 @@ const CompanyDataAccess = {
     },
     async updateById(id,body) {
         return await Company.findByIdAndUpdate({ _id: id }, body)
+    },
+    async listAll() {
+        return await Company.find().select('_id name year description logo createdAt updatedAt')
+    },
+    async deleteById(id) {
+        return await Company.findByIdAndDelete({ _id: id })
+    },
+    async getCompanyById(id) { 
+        return await Company.findById({_id: id}).select('_id name year description logo createdAt updatedAt')
     }
 } 
 
