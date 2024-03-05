94311bc89fe45e789e2cf69fe1920a89b1054a39
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-05 12:29:10 +0300

    routers

--- NLayer/router/company.router.js
+++ NLayer/router/company.router.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/index')
+const companyValidator = require('../validations/index')
 router.get('/all', controller.companyController.getAllCompany)
-router.get('/getById/:companyId', controller.companyController.getCompanyById)
-router.post('/create', controller.companyController.createCompany)
-router.post('/uploadLogo',controller.companyController.uploadLogo)
-router.put('/update/:companyId', controller.companyController.updateCompany)
-router.delete('/delete/:companyId', controller.companyController.deleteCompanyById)
+router.get('/getById/:id', [companyValidator.companyValidator.validateFindById()] , controller.companyController.getCompanyById)
+router.post('/create',[companyValidator.companyValidator.validateCreateCompany()] ,controller.companyController.createCompany)
+router.post('/uploadLogo',[companyValidator.companyValidator.validateUploadLogo()] ,controller.companyController.uploadLogo)
+router.put('/update/:id',[companyValidator.companyValidator.validateUpdateCompany()] ,controller.companyController.updateCompany)
+router.delete('/delete', [companyValidator.companyValidator.validateDeleteById()] ,controller.companyController.deleteCompanyById)
 
 module.exports={
     company:router
-}
+} 
