eb9f72fb4f92a65fdb2616f6d797fedc7d8ae6dc
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-05 16:58:46 +0300

    title cruds

--- NLayer/router/titles.router.js
+++ NLayer/router/titles.router.js
@@ -4,10 +4,10 @@ const controller = require('../controllers/index')
 const titleValidator = require('../validations/index')
 
 router.get('/all', controller.titlesController.getAllTitles)
-router.get('/getById/:titleId', controller.titlesController.getTitleById)
+router.get('/getById/:id',[titleValidator.titleValidator.validateFindById()] ,controller.titlesController.getTitleById)
 router.post('/create',[titleValidator.titleValidator.validateCreateTitle()] ,controller.titlesController.createTitle)
-router.put('/update/:titleId', controller.titlesController.updateTitle)
-router.delete('/delete/:titleId', controller.titlesController.deleteTitleById)
+router.put('/update/:id',[titleValidator.titleValidator.validateUpdateTitle()] ,controller.titlesController.updateTitle)
+router.delete('/delete',[titleValidator.titleValidator.validateDeleteById()] ,controller.titlesController.deleteTitleById)
 
 module.exports={
     titles:router
