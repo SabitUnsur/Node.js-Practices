8ffa6a181ba3c5f695279cb9abc86e05c8755505
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-05 12:54:00 +0300

    titles router

--- NLayer/router/company.router.js
+++ NLayer/router/company.router.js
@@ -6,6 +6,7 @@ router.get('/all', controller.companyController.getAllCompany)
 router.get('/getById/:id', [companyValidator.companyValidator.validateFindById()] , controller.companyController.getCompanyById)
 router.post('/create',[companyValidator.companyValidator.validateCreateCompany()] ,controller.companyController.createCompany)
 router.post('/uploadLogo',[companyValidator.companyValidator.validateUploadLogo()] ,controller.companyController.uploadLogo)
+router.post('/updateLogo',[companyValidator.companyValidator.validateUpdateLogo()] ,controller.companyController.updateLogo)
 router.put('/update/:id',[companyValidator.companyValidator.validateUpdateCompany()] ,controller.companyController.updateCompany)
 router.delete('/delete', [companyValidator.companyValidator.validateDeleteById()] ,controller.companyController.deleteCompanyById)
 
--- NLayer/router/titles.router.js
+++ NLayer/router/titles.router.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/index')
+const titleValidator = require('../validations/index')
 
 router.get('/all', controller.titlesController.getAllTitles)
 router.get('/getById/:titleId', controller.titlesController.getTitleById)
-router.post('/create', controller.titlesController.createTitle)
+router.post('/create',[titleValidator.titleValidator.validateCreateTitle()] ,controller.titlesController.createTitle)
 router.put('/update/:titleId', controller.titlesController.updateTitle)
 router.delete('/delete/:titleId', controller.titlesController.deleteTitleById)
 
