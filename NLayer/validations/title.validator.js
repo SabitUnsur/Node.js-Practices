21992bfb922daa62359a93c4b2c381b8ae853b86
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-05 16:58:34 +0300

    title cruds

--- NLayer/validations/title.validator.js
+++ NLayer/validations/title.validator.js
@@ -1,9 +1,18 @@
-const {body} = require('express-validator');
+const {body,param,query} = require('express-validator');
 
 const TitleValidator = { 
     validateCreateTitle(){
         return [body('name').not().isEmpty({ignore_whitespace: true})]
-    }
+    },
+    validateFindById(){
+        return [param('id').isMongoId()]
+    },
+    validateUpdateTitle(){
+        return [param('id').isMongoId().withMessage('Invalid ID'),body('name').not().isEmpty({ignore_whitespace: true}).withMessage('Name is required')]
+    },
+    validateDeleteById(){
+        return [query('id').isMongoId().withMessage('Invalid title id')]
+    },
 }
 
 module.exports = TitleValidator
