2f73e891fc0d1ea53de5ddc8d66c8fc640ccba47
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-06 11:10:42 +0300

    person avatar

--- NLayer/services/person.service.js
+++ NLayer/services/person.service.js
@@ -2,6 +2,7 @@ const Person = require('../models/persons.model')
 const personDal = require('../dal/index')
 const utils = require('../utils/index')
 const personDto = require('../dto/person.dto')
+const fileService = require('./file.service')
 
 
 exports.createPerson = async (req) => {
@@ -37,9 +38,25 @@ exports.createPerson = async (req) => {
             cvFile:""
         })
         const json = await personDal.person.create(person)
-        return  json
-        
-
+        return  {
+            ...personDto,
+            name: json.name,
+            surname: json.surname,
+            birthDate: new Date(json.birthDate),
+            gender:json.gender,
+            salary:  new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'YTL' }).format(json.salary),
+            tcNumber: json.tcNumber,
+            email: json.email,
+            country: json.country,
+            city: json.city,
+            company: json.company,
+            title: json.title,
+            avatar: json.avatar,
+            cvFile: json.cvFile,
+            id: json.id,
+            createdAt: json.createdAt,
+            updatedAt: json.updatedAt
+        }
     } catch (error) {
         throw new Error(error) // throw error to be caught by the controller
     }
@@ -51,5 +68,34 @@ exports.findByEmail = async (email) => {
         return person
     } catch (error) {
         throw new Error(error)
+    }
+}
+
+exports.uploadAvatar = async (req) => {
+    try {
+        const { id } = req.query
+        const str = await fileService.uploadFile(req)
+        const json = await personDal.person.updateById(id, { avatar: str })
+        return  {
+            ...personDto,
+            name: json.name,
+            surname: json.surname,
+            birthDate: new Date(json.birthDate),
+            gender:json.gender,
+            salary:  new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'YTL' }).format(json.salary),
+            tcNumber: json.tcNumber,
+            email: json.email,
+            country: json.country,
+            city: json.city,
+            company: json.company,
+            title: json.title,
+            avatar: str,
+            cvFile: json.cvFile,
+            id: json.id,
+            createdAt: json.createdAt,
+            updatedAt: json.updatedAt
+        }
+    } catch (error) {
+        throw new Error(error) // throw error to be caught by the controller
     }
 }
