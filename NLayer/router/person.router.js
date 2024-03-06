4b8b1c5baf4025ec3ad2d4257aa0c04abc79b715
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-06 12:12:13 +0300

    populate

--- NLayer/router/person.router.js
+++ NLayer/router/person.router.js
@@ -7,9 +7,10 @@ router.get('/all', controller.personController.getAllPersons)
 router.get('/getById/:personId', controller.personController.getPersonById)
 router.post('/create',[personValidator.personValidator.validateCreatePerson()],controller.personController.createPerson)
 router.put('/update/:personId', controller.personController.updatePerson)
+router.post('/uploadCv',[personValidator.personValidator.validateUploadCv()] ,controller.personController.uploadCv)
 router.post('/uploadAvatar',[personValidator.personValidator.validateUploadAvatar()] ,controller.personController.uploadAvatar)
 router.delete('/delete/:personId', controller.personController.deletePersonById)
-
+router.get('/getCompany/:id', controller.personController.getCompanyByPersonId)
 
 module.exports = {
     person: router
