d6d5166a13da9c383fcef55c0f56493d3e135db6
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-06 11:11:33 +0300

    avatar router

--- NLayer/router/person.router.js
+++ NLayer/router/person.router.js
@@ -7,6 +7,7 @@ router.get('/all', controller.personController.getAllPersons)
 router.get('/getById/:personId', controller.personController.getPersonById)
 router.post('/create',[personValidator.personValidator.validateCreatePerson()],controller.personController.createPerson)
 router.put('/update/:personId', controller.personController.updatePerson)
+router.post('/uploadAvatar',[personValidator.personValidator.validateUploadAvatar()] ,controller.personController.uploadAvatar)
 router.delete('/delete/:personId', controller.personController.deletePersonById)
 
 
