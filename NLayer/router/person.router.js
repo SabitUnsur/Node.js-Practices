c23658d4db1dc1626997efdafd72821381b750be
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-06 10:49:36 +0300

    person routers

--- NLayer/router/person.router.js
+++ NLayer/router/person.router.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/index')
+const personValidator = require('../validations/index')
 
 router.get('/all', controller.personController.getAllPersons)
 router.get('/getById/:personId', controller.personController.getPersonById)
-router.post('/create', controller.personController.createPerson)
+router.post('/create',[personValidator.personValidator.validateCreatePerson()],controller.personController.createPerson)
 router.put('/update/:personId', controller.personController.updatePerson)
 router.delete('/delete/:personId', controller.personController.deletePersonById)
 
