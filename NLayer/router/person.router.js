559369fafb168010f6232d31cb3681f346e121e8
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-06 12:36:34 +0300

    populate

--- NLayer/router/person.router.js
+++ NLayer/router/person.router.js
@@ -4,13 +4,14 @@ const controller = require('../controllers/index')
 const personValidator = require('../validations/index')
 
 router.get('/all', controller.personController.getAllPersons)
-router.get('/getById/:personId', controller.personController.getPersonById)
+router.get('/getById/:id',[personValidator.personValidator.validateGetPersonById()] ,controller.personController.getPersonById)
 router.post('/create',[personValidator.personValidator.validateCreatePerson()],controller.personController.createPerson)
-router.put('/update/:personId', controller.personController.updatePerson)
+router.put('/update/:id', controller.personController.updatePerson)
 router.post('/uploadCv',[personValidator.personValidator.validateUploadCv()] ,controller.personController.uploadCv)
 router.post('/uploadAvatar',[personValidator.personValidator.validateUploadAvatar()] ,controller.personController.uploadAvatar)
-router.delete('/delete/:personId', controller.personController.deletePersonById)
-router.get('/getCompany/:id', controller.personController.getCompanyByPersonId)
+router.delete('/delete/:id', controller.personController.deletePersonById)
+router.get('/getCompany/:id',[personValidator.personValidator.validateGetCompanyByPersonId()] ,controller.personController.getCompanyByPersonId)
+router.get('/getTitle/:id',[personValidator.personValidator.validateGetTitleByPersonId()] ,controller.personController.getTitleByPersonId)
 
 module.exports = {
     person: router
