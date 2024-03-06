98916b12634607e49cd624100a9437c71d3df5d1
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-06 10:49:01 +0300

    person controller

--- NLayer/controllers/person.controller.js
+++ NLayer/controllers/person.controller.js
@@ -1,8 +1,37 @@
+const personService = require('../services/index')
+const { StatusCodes } = require('http-status-codes')
+const baseResponse = require('../dto/baseresponse.dto')
+const utils = require('../utils/index')
+
 exports.getAllPersons= (req,res) => {}
 
 exports.getPersonById= (req,res) => { }
 
-exports.createPerson= (req,res) => { }
+exports.createPerson= async (req,res) => {
+    try {
+        const isInvalid = utils.helpers.handleValidation(req)
+        if (isInvalid) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                ...baseResponse,
+                ...isInvalid
+            })
+        }
+        const json = await personService.person.createPerson(req)
+        res.status(StatusCodes.CREATED).json({ ...baseResponse, data: json, success: true, timestamp: Date.now(), code: StatusCodes.CREATED, message: 'Person created successfully' })
+
+
+    } catch (error) {
+        utils.helpers.logToError(error, req)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            ...baseResponse,
+            error: true,
+            success: false,
+            timestamp: Date.now(),
+            code: StatusCodes.INTERNAL_SERVER_ERROR,
+            message: error.message
+        })
+    }
+ }
 
 exports.updatePerson= (req,res) => { }
 
