a81fc58f3c697ecdcc056130e8986c3c32df1943
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-07 11:55:17 +0300

    relations

--- NLayer/services/person.service.js
+++ NLayer/services/person.service.js
@@ -3,6 +3,8 @@ const personDal = require('../dal/index')
 const utils = require('../utils/index')
 const personDto = require('../dto/person.dto')
 const fileService = require('./file.service')
+const titleDal = require('../dal/index') 
+const companyDal = require('../dal/index')
 const personCompanyDto = require('../dto/person.company.dto')
 const personTitleDto = require('../dto/person.title.dto')
 
@@ -22,6 +24,8 @@ exports.createPerson = async (req) => {
             company,
             title
         } = req.body
+        const findedTitle = await titleDal.title.getTitleById(title) 
+        const findedCompany = await companyDal.company.getCompanyById(company)
         const person = new Person({
             name,
             surname,
@@ -39,6 +43,10 @@ exports.createPerson = async (req) => {
             cvFile: ""
         })
         const json = await personDal.person.create(person)
+        findedTitle.persons.push(json._id)
+        findedCompany.persons.push(json._id)
+        await companyDal.company.create(findedCompany)
+        await titleDal.title.create(findedTitle)
         return {
             ...personDto,
             name: json.name,
@@ -101,6 +109,40 @@ exports.uploadAvatar = async (req) => {
     }
 }
 
+exports.updateAvatar = async (req) => { 
+    try {
+        const { id } = req.query
+        const str = await fileService.uploadImage(req)
+        const findedPerson = await personDal.person.getPersonById(id)
+        const isAvatarDeleted = utils.helpers.deleteFromDisk(findedPerson.avatar ? findedPerson.avatar.split('uploads/')[1] : '')
+        if(isAvatarDeleted){ 
+            const json = await personDal.person.updateById(id, { avatar: str })
+            return {
+                ...personDto,
+                name: json.name,
+                surname: json.surname,
+                birthDate: new Date(json.birthDate),
+                gender: json.gender,
+                salary: new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'YTL' }).format(json.salary),
+                tcNumber: json.tcNumber,
+                email: json.email,
+                country: json.country,
+                city: json.city,
+                company: json.company,
+                title: json.title,
+                avatar: str,
+                cvFile: json.cvFile,
+                id: json.id,
+                createdAt: json.createdAt,
+                updatedAt: json.updatedAt
+            }
+        }
+        throw new Error('Logo could not be updated')
+    } catch (error) {
+        throw new Error(error) // throw error to be caught by the controller
+    }
+}
+
 
 exports.uploadCv = async (req) => {
     try {
@@ -130,6 +172,42 @@ exports.uploadCv = async (req) => {
         throw new Error(error) // throw error to be caught by the controller
     }
 }
+
+
+exports.updateCv = async (req) => {
+    try {
+        const { id } = req.query
+        const str = await fileService.uploadCv(req)
+        const findedPerson = await personDal.person.getPersonById(id)
+        const isCvDeleted = utils.helpers.deleteFromDisk(findedPerson.cvFile ? findedPerson.cvFile.split('uploads/')[1] : '')
+        if(isCvDeleted){ 
+            const json = await personDal.person.updateById(id, { cvFile: str })
+            return {
+                ...personDto,
+                name: json.name,
+                surname: json.surname,
+                birthDate: new Date(json.birthDate),
+                gender: json.gender,
+                salary: new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'YTL' }).format(json.salary),
+                tcNumber: json.tcNumber,
+                email: json.email,
+                country: json.country,
+                city: json.city,
+                company: json.company,
+                title: json.title,
+                avatar: json.avatar,
+                cvFile: str,
+                id: json.id,
+                createdAt: json.createdAt,
+                updatedAt: json.updatedAt
+            }
+        }
+        throw new Error('Logo could not be updated')
+    } catch (error) {
+        throw new Error(error) // throw error to be caught by the controller
+    }
+}
+
 
 exports.getCompanyByPersonId = async (req) => {
     try {
@@ -222,7 +300,6 @@ exports.getAllListWithPagination = async (req) => {
     }
 }
 
-
 exports.updatePerson = async (req) => {
     try {
         const {
@@ -256,6 +333,7 @@ exports.updatePerson = async (req) => {
         })
         return {
             ...personDto,
+            id: json._id,
             name,
             surname,
             birthDate: new Date(birthDate),
@@ -265,14 +343,45 @@ exports.updatePerson = async (req) => {
             email: email,
             country: country,
             city: city,
-            avatar: avatar,
-            cvFile: vFile,
-            id: id,
-            createdAt: createdAt,
-            updatedAt: updatedAt
+            company: company,
+            title: title,
+            avatar: json.avatar,
+            cvFile: json.cvFile,
         }
 
     } catch (error) {
         throw new Error(error) // throw error to be caught by the controller
+    }
+}
+
+exports.deletePersonById = async (req) => {
+    try {
+        const { id } = req.query
+        const findedPerson = await personDal.person.getPersonById(id)
+        const isAvatarDeleted = utils.helpers.deleteFromDisk(findedPerson.avatar ? findedPerson.avatar.split('uploads/')[1] : '')
+        const isCvDeleted = utils.helpers.deleteFromDisk(findedPerson.avatar ? findedPerson.cvFile.split('uploads/')[1] : '')
+        if (isAvatarDeleted && isCvDeleted) {
+            const json = await personDal.person.deleteById(id)
+            return {
+                ...personDto,
+                name: json.name,
+                surname: json.surname,
+                birthDate: new Date(json.birthDate),
+                gender: json.gender,
+                salary: new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'YTL' }).format(json.salary),
+                tcNumber: json.tcNumber,
+                email: json.email,
+                country: json.country,
+                city: json.city,
+                avatar: json.avatar,
+                cvFile: json.cvFile,
+                id: json.id,
+                createdAt: json.createdAt,
+                updatedAt: json.updatedAt
+            }
+        }
+        throw new Error('Logo could not be deleted')
+    } catch (error) {
+        throw new Error(error)
     }
 }
