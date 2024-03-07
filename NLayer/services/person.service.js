19ee414a4f8e04e16d1057be6b4f9e0ca5ebf9c8
Egemen Unsur <sabitozbilgic2306@gmail.com>
2024-03-07 10:46:44 +0300

    updates

--- NLayer/services/person.service.js
+++ NLayer/services/person.service.js
@@ -185,20 +185,94 @@ exports.getPersonById = async (req) => {
     }
 }
 
-exports.getAllList = async() => {
+exports.getAllList = async () => {
     try {
         const json = await personDal.person.listAll({}, [{
-                path: 'company',
-                select: 'company _id year name',
-                //match: { year: { $gte: 2010 } } // filter by year greater than or equal to 2010 
-            },
-            {
-                path: 'title',
-                select: '_id name'
-            }
+            path: 'company',
+            select: 'company _id year name',
+            //match: { year: { $gte: 2010 } } // filter by year greater than or equal to 2010 
+        },
+        {
+            path: 'title',
+            select: '_id name'
+        }
         ])
         return json
     } catch (error) {
         throw new Error(error)
+    }
+}
+
+exports.getAllListWithPagination = async (req) => {
+    try {
+        const { perPage, page, sortBy, sortDirection } = req.query
+        const json = await personDal.person.listAllWithPagination({}, [{
+            path: 'company',
+            select: 'company _id year name',
+            //match: { year: { $gte: 2010 } } // filter by year greater than or equal to 2010 
+        },
+        {
+            path: 'title',
+            select: '_id name'
+        }
+        ], perPage, perPage * (page - 1), { [sortBy]: sortDirection })  // [sortBy]:sortDirection => amacı dinamik olarak sortBy ve sortDirection değerlerini almak için kullanılır
+        return json
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+
+exports.updatePerson = async (req) => {
+    try {
+        const {
+            name,
+            surname,
+            birthDate,
+            gender,
+            salary,
+            tcNumber,
+            email,
+            password,
+            country,
+            city,
+            company,
+            title
+        } = req.body
+        const { id } = req.params
+        const json = await personDal.person.updateById(id, {
+            name,
+            surname,
+            birthDate,
+            gender,
+            salary,
+            tcNumber,
+            email,
+            password: utils.helpers.hashToPassword(password),
+            country,
+            city,
+            company,
+            title
+        })
+        return {
+            ...personDto,
+            name,
+            surname,
+            birthDate: new Date(birthDate),
+            gender: gender,
+            salary: new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'YTL' }).format(salary),
+            tcNumber: tcNumber,
+            email: email,
+            country: country,
+            city: city,
+            avatar: avatar,
+            cvFile: vFile,
+            id: id,
+            createdAt: createdAt,
+            updatedAt: updatedAt
+        }
+
+    } catch (error) {
+        throw new Error(error) // throw error to be caught by the controller
     }
 }
